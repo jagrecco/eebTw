f60adb413359316a2d138690511eb686754de2c4
jagrecco <96453177+jagrecco@users.noreply.github.com>
2023-08-23 12:30:42 -0300

    Hook estado hamburguesa ok

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,9 +1,18 @@
 import hamb from "../../assets/hamb.svg";
+import { useState } from "react";
 
 export const NavBar = () => {
+
+  const [clicked, setClicked]=useState(false);
+
+  const handleClick=()=>{
+    setClicked(prevState=>!prevState)
+    console.log(clicked)
+  }
+  
   return (
     <nav className="">
-      <ul className="bg-sky-100 w-[329px] h-[185px] top-[120px] absolute left-0 right-0 mx-auto flex flex-col items-center place-content-around after:content-[''] after:absolute after:top-[-24px] after:right-[0px] after:border-[12px] after:border-sky-100 after:border-t-transparent after:border-l-transparent">
+      <ul className="bg-sky-100 w-[329px] h-[185px] top-[120px] absolute left-0 right-0 mx-auto  flex-col items-center place-content-around after:content-[''] after:absolute after:top-[-24px] after:right-[0px] after:border-[12px] after:border-sky-100 after:border-t-transparent after:border-l-transparent  hidden">
         <li className="list-none font-light hover:cursor-pointer hover:font-bold hover:text-sky-700">
           Primer
         </li>
@@ -14,8 +23,8 @@ export const NavBar = () => {
           Tercer
         </li>
       </ul>
-      <div id="hammer">
-        <img className="hover:cursor-pointer" src={hamb} alt="" />
+      <div className="hover:cursor-pointer" onClick={handleClick}>
+        <img  src={hamb} alt="Menú" />
       </div>
     </nav>
   );
